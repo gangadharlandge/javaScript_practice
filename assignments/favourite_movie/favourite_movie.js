c4108a92f8c7371e2f7c37e0e1b8901d184c6eb7
Gangadhar landge <66439978+gangadharlandge@users.noreply.github.com>
2021-11-25 09:26:37 -0800

    Update favourite_movie.js
    making changes in fevourite_movie.js file

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,22 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
-}
+    if (operation == "add"){
+        movies.push(movie);
+        return movies;
+
+    }
+    if (operation == "remove"){
+        if (movies.length > 0){
+            movies.pop();
+            return movies;
+        }
+        else{
+            return movies;
+    }
+
+    }
+
+    }
 
 module.exports = favouriteMovie;
